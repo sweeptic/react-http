847dad1d8ceabc0dd21e17645c9ad823f8d028b1
Attila <59293104+sweeptic@users.noreply.github.com>
2020-05-08 21:48:28 +0200

    add axios and fetch method

--- src/Container/Blog.js
+++ src/Container/Blog.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
-import Post from './../Component/Post';
-import FullPost from './../Component/FullPost';
-import NewPost from './../Component/NewPost';
+import Post from '../Component/Post';
+import FullPost from '../Component/FullPost';
+import NewPost from '../Component/NewPost';
 import Container from 'react-bootstrap/Container'
+import Axios from 'axios';
 
 
 class Blog extends Component {
+
+   state = {
+      posts: []
+   };
+
+
+   componentDidMount() {
+      Axios
+         .get("https://jsonplaceholder.typicode.com/posts")
+         .then((response) => {
+            this.setState({
+               posts: response.data,
+            });
+             console.log(response);
+         });
+   }
+
    render() {
+
       return (
          <div>
             <Container className="bg-light p-3 card-columns" >
@@ -28,7 +47,16 @@ class Blog extends Component {
             </Container>
          </div>
       )
+
+      // componentDidMount() {
+      //    console.log(componentDidMount)
+      // }
+
+
+
    }
+
+
 }
 
 export default Blog
