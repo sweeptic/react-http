fff1bb710805e9df09e9fde7927114f7eebd8eb3
Attila <59293104+sweeptic@users.noreply.github.com>
2020-05-08 13:38:14 +0200

    add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import Post from './Component/Post';
+import Container from 'react-bootstrap/Container'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <Container className="bg-light p-3 card-columns" >
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </Container>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/Component/Post.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div className="card border-primary mb-3">
+            <div className="card-body">
+                <h4 className="card-title">Title</h4>
+                <p className="card-text">Lorem ipsum dolor sit amet.</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Post
